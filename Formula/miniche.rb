a6b72a6234d1c3967c46ed5b307058c9a88ef8f2
goreleaserbot <goreleaser@carlosbecker.com>
2018-09-07 23:02:45 +0300

    Brew formula update for miniche version v0.1.5

--- Formula/miniche.rb
+++ Formula/miniche.rb
@@ -1,9 +1,9 @@
 class Miniche < Formula
   desc "miniche"
   homepage "https://eclipse.com/che"
-  url "https://github.com/skabashnyuk/miniche/releases/download/v0.1.4/miniche-0.1.4-darwin-amd64.tar.gz"
-  version "0.1.4"
-  sha256 "cab5cf5460a403de56ad120b61c2c172d1d5c38ba8307439aa7fb50448ae0017"
+  url "https://github.com/skabashnyuk/miniche/releases/download/v0.1.5/miniche-0.1.5-darwin-amd64.tar.gz"
+  version "0.1.5"
+  sha256 "49a342c6da923187904695febafbaeedd703f02672e89871fa3a7319392d7916"
 
   def install
     bin.install "miniche"
